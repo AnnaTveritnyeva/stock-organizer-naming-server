5e135888812d9247bd2ea30bc5bccce1a3def76a
AnnaTveritnyeva <atveretniv@gmail.com>
2022-12-04 19:33:06 +0200

    Initial commit

---
+++ src/main/java/com/stockorganizer/stockorganizernamingserver/StockOrganizerNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.stockorganizer.stockorganizernamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOrganizerNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOrganizerNamingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockorganizer/stockorganizernamingserver/StockOrganizerNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockorganizer.stockorganizernamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrganizerNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
